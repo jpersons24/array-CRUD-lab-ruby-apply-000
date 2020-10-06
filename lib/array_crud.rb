48a245e9af9b9c4cabf7eb70b41c1365f9d0d559
carefree-window-8613 <jakobep24@gmail.com>
2020-10-06 14:25:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  add_element_to_end_of_array(array, element).pop
+  array = ["I", "am", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
