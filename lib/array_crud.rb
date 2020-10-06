7845e7bf7368812eaabf60a82a48f848d5f20a07
carefree-window-8613 <jakobep24@gmail.com>
2020-10-06 14:30:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,7 +26,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array = ["wow", "I", "am", "learning", "arrays!"]
-  index_number = [2]
+  index_number = 2
 end
 
 def retrieve_first_element_from_array(array)
