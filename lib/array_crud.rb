fb9f098789b2b2817d644d125de8d08fbeb7c10d
carefree-window-8613 <jakobep24@gmail.com>
2020-10-06 14:27:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,7 +20,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  create_an_array.shift(1)
+  array = ["wow", "I", "am", "learning", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
